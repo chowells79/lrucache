b062dafec57ee1e901a4cd7f5b6474eb962b3324
Carl Howells <chowells@others.local>
2010-02-23 00:28:24 -0800

    Update cabal file to 0.2.0.1

---
+++ MemTest.hs
@@ -0,0 +1,25 @@
+import Prelude hiding ( lookup )
+
+import Control.Monad
+import Control.Concurrent
+import Data.IORef
+
+import Data.Cache.LRU.IO.Internal
+
+main :: IO ()
+main = do
+  v1 <- newAtomicLRU 10 -- for endless inserts
+  v2 <- newAtomicLRU 10 -- for endless lookups (miss)
+  v3 <- newAtomicLRU 10 -- for endless lookups (hit)
+
+  counter <- newIORef (0 :: Int)
+
+  insert 1 "bar" v3
+
+  forever $ do
+         c <- readIORef counter
+         writeIORef counter $ c + 1
+
+         insert c (show c) v1
+         lookup (1 :: Int) v2
+         lookup (1 :: Int) v3
