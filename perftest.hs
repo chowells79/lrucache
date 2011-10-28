32e7b7cb21c6f39a659539c0dbcab057025a20cb
Carl Howells <chowells79@gmail.com>
2011-10-28 14:54:45 -0700

    run more samples when benchmarking

--- perftest.hs
+++ perftest.hs
@@ -3,18 +3,23 @@ import Criterion.Main
 
 import Data.Cache.LRU.Capacity
 import Data.Cache.LRU
+import Data.Monoid
 
 import System.Random
 
 main :: IO ()
 main = do
-    let config = defaultConfig
+    let config = defaultConfig {
+                   cfgSamples   = Last $ Just 200
+                 , cfgPerformGC = Last $ Just True
+                 }
         setup  = return ()
         tests  = [bench "mass insert" massInsertTest]
 
     defaultMainWith config setup tests
 
 
+-- make this a CAF, in order to only evaluate it once
 massInsertList :: [(Int, Int)]
 massInsertList = take 10000 $ zip keys vals
   where
